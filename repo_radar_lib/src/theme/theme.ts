5316a54b93133e4feee212649ade33972b839180
Ahmed Waseem <ahmedwaseemr@aucegypt.edu>
2026-09-17 19:03:53 +0300

    removed placeholder assets, added dark theme support to lib, stripped placeholder page

--- repo_radar_lib/src/theme/theme.ts
+++ repo_radar_lib/src/theme/theme.ts
@@ -1,12 +1,29 @@
 import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
+  cssVariables: {
+    colorSchemeSelector: 'media',
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: '#1976d2',
+        },
+        secondary: {
+          main: '#9c27b0',
+        },
+      },
     },
-    secondary: {
-      main: '#9c27b0',
+    dark: {
+      palette: {
+        primary: {
+          main: '#1976d2',
+        },
+        secondary: {
+          main: '#9c27b0',
+        },
+      },
     },
   },
 })
